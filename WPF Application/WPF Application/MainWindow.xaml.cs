e6d9197412ee8b8e837d41484371ec6edd15b547
Tadeáš Zeman <zemant23@spstrutnov.cz>
2025-02-12 14:58:10 +0100

    graficke rozhrani prvni tlaicitka

---
+++ WPF Application/WPF Application/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Application   
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // citac
+        private int pocitadlo = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kliknuti1(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti2(object sender, RoutedEventArgs e)
+        {
+            string k1 = "%";
+            Popisek.Content = pocitadlo.ToString();
+            Popisek.Content += "%";
+
+        }
+        private void Kliknuti3(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti4(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti5(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti6(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti7(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti8(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti9(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti10(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti11(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti12(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti13(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti14(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti15(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti16(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti17(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti18(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti19(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti20(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti21(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti22(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti23(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+        private void Kliknuti24(object sender, RoutedEventArgs e)
+        {
+            pocitadlo += 1;
+            Popisek.Content = pocitadlo.ToString();
+
+        }
+    }
+}
