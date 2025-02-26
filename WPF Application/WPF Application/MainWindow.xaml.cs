f0ab97d2f904c51f55ddfc0f476537e19d25362b
Tadeáš Zeman <zemant23@spstrutnov.cz>
2025-02-26 13:54:19 +0100

    klalkulačka hotova

--- WPF Application/WPF Application/MainWindow.xaml.cs
+++ WPF Application/WPF Application/MainWindow.xaml.cs
@@ -9,165 +9,224 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_Application   
+namespace WPF_Application
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        // citac
-        private int pocitadlo = 0;
+        private double _currentValue = 0;
+        private double _lastValue = 0;
+        private string _lastOperator = string.Empty;
+        private bool _isOperatorClicked = false;
+        private bool _isDecimalAdded = false;
 
-        public MainWindow()
+        public MainWindow() 
         {
             InitializeComponent();
         }
 
-        private void Kliknuti2(object sender, RoutedEventArgs e)
+        // Kliknutí na tlačítka číslic
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
+            var button = sender as Button;
+            if (button != null)
+            {
+                string content = button.Content.ToString();
+                if (_isOperatorClicked)
+                {
+                    Display.Text = content;
+                    _isOperatorClicked = false;
+                }
+                else
+                {
+                    if (Display.Text == "0")
+                    {
+                        Display.Text = content;
+                    }
+                    else
+                    {
+                        Display.Text += content;
+                    }
+                }
+            }
         }
-        private void Kliknuti1(object sender, RoutedEventArgs e)
-        {
-            string k1 = "%";
-            Popisek.Content = pocitadlo.ToString();
-            Popisek.Content += "%";
 
+        // Operátory +, -, *, /
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                string operatorSymbol = button.Content.ToString();
+                if (operatorSymbol == "=")
+                {
+                    PerformCalculation();
+                }
+                else
+                {
+                    _lastOperator = operatorSymbol;
+                    _lastValue = double.Parse(Display.Text);
+                    _isOperatorClicked = true;
+                    _isDecimalAdded = false;
+                }
+            }
         }
-        private void Kliknuti3(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Provádí výpočet na základě posledního operátoru
+        private void PerformCalculation()
+        {
+            double result = 0;
+            double currentValue = double.Parse(Display.Text);
+
+            switch (_lastOperator)
+            {
+                case "+":
+                    result = _lastValue + currentValue;
+                    break;
+                case "-":
+                    result = _lastValue - currentValue;
+                    break;
+                case "*":
+                    result = _lastValue * currentValue;
+                    break;
+                case "/":
+                    if (currentValue != 0)
+                    {
+                        result = _lastValue / currentValue;
+                    }
+                    else
+                    {
+                        Display.Text = "Error";
+                        return;
+                    }
+                    break;
+                case "%":
+                    result = _lastValue * currentValue / 100;
+                    break;
+                case "1/x":
+                    if (currentValue != 0)
+                    {
+                        result = 1 / currentValue;
+                    }
+                    else
+                    {
+                        Display.Text = "Error";
+                        return;
+                    }
+                    break;
+                case "x²":
+                    result = currentValue * currentValue;
+                    break;
+                case "√x":
+                    if (currentValue >= 0)
+                    {
+                        result = Math.Sqrt(currentValue);
+                    }
+                    else
+                    {
+                        Display.Text = "Error";
+                        return;
+                    }
+                    break;
+            }
+
+            Display.Text = result.ToString();
+            _lastValue = result;
+            _lastOperator = string.Empty;
         }
-        private void Kliknuti4(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro vymazání celé kalkulačky
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            _currentValue = 0;
+            _lastValue = 0;
+            _lastOperator = string.Empty;
+            Display.Text = "0";
+            _isDecimalAdded = false;
         }
-        private void Kliknuti5(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro vymazání posledního zadání
+        private void ClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Text = "0";
+            _isDecimalAdded = false;
         }
-        private void Kliknuti6(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro smazání posledního zadání (backspace)
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text.Length > 1)
+            {
+                Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+            }
+            else
+            {
+                Display.Text = "0";
+            }
         }
-        private void Kliknuti7(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro negaci čísla
+        private void Negate_Click(object sender, RoutedEventArgs e)
+        {
+            double currentValue = double.Parse(Display.Text);
+            Display.Text = (-currentValue).ToString();
         }
-        private void Kliknuti8(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro přidání desetinné čárky
+        private void Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isDecimalAdded)
+            {
+                Display.Text += ",";
+                _isDecimalAdded = true;
+            }
         }
-        private void Kliknuti9(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro výpočet 1/x
+        private void Reciprocal_Click(object sender, RoutedEventArgs e)
+        {
+            double currentValue = double.Parse(Display.Text);
+            if (currentValue != 0)
+            {
+                Display.Text = (1 / currentValue).ToString();
+            }
+            else
+            {
+                Display.Text = "Error";
+            }
         }
-        private void Kliknuti10(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro výpočet x²
+        private void Square_Click(object sender, RoutedEventArgs e)
+        {
+            double currentValue = double.Parse(Display.Text);
+            Display.Text = (currentValue * currentValue).ToString();
         }
-        private void Kliknuti11(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro výpočet √x
+        private void SquareRoot_Click(object sender, RoutedEventArgs e)
+        {
+            double currentValue = double.Parse(Display.Text);
+            if (currentValue >= 0)
+            {
+                Display.Text = Math.Sqrt(currentValue).ToString();
+            }
+            else
+            {
+                Display.Text = "Error";
+            }
         }
-        private void Kliknuti12(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
+        // Funkce pro výpočet procenta
+        private void Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            double currentValue = double.Parse(Display.Text);
+            Display.Text = (_lastValue * currentValue / 100).ToString();
         }
-        private void Kliknuti13(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
 
-        }
-        private void Kliknuti14(object sender, RoutedEventArgs e)
+        // Funkce pro rovnítko
+        private void Equal_Click(object sender, RoutedEventArgs e)
         {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti15(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti16(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti17(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti18(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti19(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti20(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti21(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti22(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti23(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
-        }
-        private void Kliknuti24(object sender, RoutedEventArgs e)
-        {
-            pocitadlo += 1;
-            Popisek.Content = pocitadlo.ToString();
-
+            PerformCalculation();
         }
     }
 }
